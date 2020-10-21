2c9f65c13051645b0bf4df6035212766817beff9
NastyaTK <tavtkachenko@gmail.com>
2020-10-21 19:45:32 +0300

    add doubleClick with RxJS

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { map, buffer, debounce, debounceTime, filter, tap } from 'rxjs/operators';
 import { GenerateDataService } from "../shared/generate-data.service";
 import { ITimer } from "../shared/time";
 
@@ -7,12 +9,24 @@ import { ITimer } from "../shared/time";
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss']
 })
-export class TimerComponent implements OnInit {
+export class TimerComponent {
   public timer = { hh: "00", mm: "00", ss: "00" };
   private intervalObj: number;
+  @ViewChild('timerElement')
+  public timerDomElement: ElementRef<HTMLDivElement>;
+
   constructor(private dataService: GenerateDataService) { }
 
-  ngOnInit(): void {
+  public ngAfterViewInit(): void {
+    const click$: Observable<Event> = fromEvent(this.timerDomElement.nativeElement, 'click');
+    const doubleclick$: Observable<Array<Event>> = click$.pipe(
+      buffer(click$.pipe(debounceTime(150))),
+      filter((list: Array<Event>) => list.length === 2)
+    )
+
+    doubleclick$.subscribe(() => {
+      this.onPause();
+    })
   }
 
   public onStart() {
@@ -37,7 +51,7 @@ export class TimerComponent implements OnInit {
       this.onStart();
     }
   }
-  public onPause() {
+  private onPause() {
     if (this.intervalObj) {
       window.clearInterval(this.intervalObj);
       this.intervalObj = undefined;
